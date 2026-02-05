9c4ae8c2329201461f84251b27582d68bac320a7
Truuuuuuuu <jethruel@gmail.com>
2026-02-05 23:02:48 +0800

    last commit for this tuts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Example</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+
+    @vite(['resources/js/app.js'])
+
 </head>
 <body class="h-full">
 <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
